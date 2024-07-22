17c04fbd51addbfa0a66c12714bb33132d67428c
Roshani-Poojari <roshanipoojari2002@gmail.com>
2024-07-22 19:31:43 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileHandlingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\roshani.poojari\Desktop\Training\FileHandlingApp\assets\Usernames.txt";
+            Console.WriteLine("Enter your username: ");
+            string name = Console.ReadLine();
+            using (StreamWriter sw = new StreamWriter(file, true))
+            {
+                sw.WriteLine(name);
+            }
+        }
+    }
+}
